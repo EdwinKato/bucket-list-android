de1c8551063c93a33ff83489097ef8a8e722d483
Edwin Kato <edwin.kato@andela.com>
2017-10-18 12:08:51 +0300

    Add Item model

---
+++ app/src/main/java/com/dev4tomorrow/edwinkato/bucketlist/model/Item.java
@@ -0,0 +1,41 @@
+package com.dev4tomorrow.edwinkato.bucketlist.model;
+
+/**
+ * Created by edwinkato on 10/18/17.
+ */
+
+public class Item {
+    int id;
+    String title;
+    String description;
+
+    public Item(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
