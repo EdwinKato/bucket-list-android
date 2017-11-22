a9d6c6c3cf9982ca64594f6f70bf85353c74c9b3
Edwin Kato <edwin.kato@andela.com>
2017-11-22 14:05:05 +0300

    Add newEdit bucket list activity

---
+++ app/src/main/java/com/edwinkato/bucketlist/ui/newEditbucketList/NewEditBucketListActivity.java
@@ -0,0 +1,13 @@
+package com.edwinkato.bucketlist.ui.newEditbucketList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewEditBucketListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_edit_bucket_list);
+    }
+}
