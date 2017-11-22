0bb1df125446c527cbce1023fe7de3d9fc38135f
Edwin Kato <edwin.kato@andela.com>
2017-11-22 17:54:03 +0300

    Edit style for new edit bucketlist

--- app/src/main/java/com/edwinkato/bucketlist/ui/bucketLists/BucketListsActivity.java
+++ app/src/main/java/com/edwinkato/bucketlist/ui/bucketLists/BucketListsActivity.java
@@ -2,25 +2,15 @@ package com.edwinkato.bucketlist.ui.bucketLists;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.edwinkato.bucketlist.R;
 import com.edwinkato.bucketlist.ui.LoginActivity;
+import com.edwinkato.bucketlist.ui.newEditbucketList.NewEditBucketListActivity;
 import com.edwinkato.bucketlist.ui.profile.ProfileActivity;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.ResultCodes;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,8 +21,8 @@ public class BucketListsActivity extends AppCompatActivity {
     public static final String TAG = "BUCKET_LISTS_ACTIVITY";
 
     private FirebaseUser user;
-    @BindView(R.id.account_picture) ImageView accountIcon;
-    @BindView(R.id.add_icon) ImageView addIcon;
+    @BindView(R.id.action_profile) ImageView accountIcon;
+    @BindView(R.id.action_add_bucket_list) ImageView addIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +32,15 @@ public class BucketListsActivity extends AppCompatActivity {
 
     }
 
-    @OnClick(R.id.account_picture)
+    @OnClick(R.id.action_profile)
     public void goToProfile(){
         Intent intent = new Intent(BucketListsActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.action_add_bucket_list)
+    public void goToNewEditBucketList(){
+        Intent intent = new Intent(BucketListsActivity.this, NewEditBucketListActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/edwinkato/bucketlist/ui/newEditbucketList/NewEditBucketListActivity.java
+++ app/src/main/java/com/edwinkato/bucketlist/ui/newEditbucketList/NewEditBucketListActivity.java
@@ -2,14 +2,42 @@ package com.edwinkato.bucketlist.ui.newEditbucketList;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.edwinkato.bucketlist.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class NewEditBucketListActivity extends AppCompatActivity {
+
+    @BindView(R.id.action_cancel)
+    ImageView actionCancel;
+
+    @BindView(R.id.action_save_bucket_list)
+    ImageView actionSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_edit_bucket_list);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.action_cancel)
+    public void closeActivity(){
+        finish();
+    }
+
+    @OnClick(R.id.action_save_bucket_list)
+    public void saveBucketList(){
+        save();
+        finish();
+    }
+
+    private void save() {
+
     }
 }
