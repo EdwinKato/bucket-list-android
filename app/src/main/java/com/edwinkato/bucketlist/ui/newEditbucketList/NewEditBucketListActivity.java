5118fc4760f8aa726e3ff7f1914e2fc2b781b832
Edwin Kato <edwin.kato@andela.com>
2017-11-22 14:30:20 +0300

    Add login layout styles

--- app/src/main/java/com/edwinkato/bucketlist/ui/newEditbucketList/NewEditBucketListActivity.java
+++ app/src/main/java/com/edwinkato/bucketlist/ui/newEditbucketList/NewEditBucketListActivity.java
@@ -3,6 +3,8 @@ package com.edwinkato.bucketlist.ui.newEditbucketList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.edwinkato.bucketlist.R;
+
 public class NewEditBucketListActivity extends AppCompatActivity {
 
     @Override
